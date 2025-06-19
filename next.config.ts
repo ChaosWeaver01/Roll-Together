6e7e20ad2706724341544a5a8df5126af3d4d17f
Ben Shields <BenShields01@gmail.com>
2025-06-19 22:04:15 +0000

    the repository is named https://chaosweaver01.github.io/studio/

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   output: 'export', // Enable static HTML export
   // If your site is hosted at a subpath, e.g., https://your-username.github.io/your-repo-name/
   // Uncomment and set the basePath and assetPrefix below.
-  // basePath: '/your-repo-name',
-  // assetPrefix: '/your-repo-name/', // Note the trailing slash for assetPrefix
+  basePath: '/studio',
+  assetPrefix: '/studio/', // Note the trailing slash for assetPrefix
   typescript: {
     ignoreBuildErrors: true,
   },
